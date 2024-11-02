4295bc1d7e8a460e0723818228b53366c58e8d53
Gábor <beresgabo2002@gmail.com>
2024-11-02 17:02:32 +0100

    Find most western column 1st version

--- src/main.py
+++ src/main.py
@@ -21,21 +21,27 @@ plt.subplots_adjust(bottom=0.2)
 start_ax = plt.axes([0.7, 0.05, 0.1, 0.075])
 start_button = Button(start_ax, 'Start')
 
-ani = None
-
 # Function to start the animation
 def start(event):
-    global ani
-    if ani is None:  # Start the animation if it hasn't started yet
-        ani = animation.FuncAnimation(
-            fig, animate, fargs=(ax, plane), interval=500, blit=False, cache_frame_data=False
-        )
-        plt.draw()
+    # global aniFirst
+    # if aniFirst is None:  # Start the animation if it hasn't started yet
+    #     aniFirst = animation.FuncAnimation(
+    #         fig, animate, fargs=(ax, plane), interval=1000, blit=False, cache_frame_data=False
+    #     )
+    #     print("Animation started")
+    #     plt.draw()
+
+    drawer.draw_hex_grid(ax, plane)
+    plane.moveActor(ax, drawer)
+
+    
+
+# def buildPar():
+#     plane.buildPar()
+#     drawer.draw_hex_grid(ax, plane)
 
 # Function to update the animation
-def animate(frame, ax, plane):
-    drawer.draw_hex_grid(ax, plane)
-    plane.moveActor()
+# def animate(frame, ax, plane):
 
 # Attach the button click event to start the animation
 start_button.on_clicked(start)
--- src/plane.py
+++ src/plane.py
@@ -1,6 +1,9 @@
+import time
 import actor
 import coordinate
+import matplotlib.pyplot as plt
 import random
+
 
 class Plane:
     actor = actor.Actor(0,0)
@@ -39,12 +42,100 @@ class Plane:
         return self.actor
     
     # Moves the Actor to the proper coordinate based on a random direction (For testing purpose only)
-    def moveActor(self):
-        if(self.actor.getPosition().getX() % 2 == 0):
-            direction = random.choice(list(coordinate.DirectionEven))
-        else :
-            direction = random.choice(list(coordinate.DirectionOdd))
+    def moveActor(self, ax, drawer):
+        # if(self.actor.getPosition().getX() % 2 == 0):
+        #     direction = random.choice(list(coordinate.DirectionEven))
+        # else :
+        #     direction = random.choice(list(coordinate.DirectionOdd))
+        # actorPosition = self.actor.getPosition()
+        # newposition = actorPosition.mix(direction.value)
+        # if self.tiles[newposition.getX()][newposition.getY()] == True:
+        #     self.actor.move(direction.value)
+
+        while True:
+        
+            if(self.actor.getPosition().getX() % 2 == 0):
+                directionFirst = coordinate.DirectionEven.NW
+                directionSecond = coordinate.DirectionEven.SW
+                directionThird = coordinate.DirectionEven.N
+            else:
+                directionFirst = coordinate.DirectionOdd.NW
+                directionSecond = coordinate.DirectionOdd.SW
+                directionThird = coordinate.DirectionOdd.N
+
+            actorPosition = self.actor.getPosition()
+            newPositionFirst = actorPosition.mix(directionFirst.value)
+            newPositionSecond = actorPosition.mix(directionSecond.value)
+            newPositionThird = actorPosition.mix(directionThird.value)
+            drawer.draw_hex_grid(ax, self)
+            plt.draw()
+            plt.pause(1)
+            if(self.getTile(newPositionFirst.getX(),newPositionFirst.getY()) == True):
+                self.actor.move(directionFirst.value)
+            elif (self.getTile(newPositionSecond.getX(),newPositionSecond.getY()) == True):
+                self.actor.move(directionSecond.value)
+            elif (self.getTile(newPositionThird.getX(),newPositionThird.getY()) == True):
+                self.actor.move(directionThird.value)
+            else:
+                firstColumn = True
+                self.buildPar(ax, drawer, firstColumn)
+
+
+    def buildPar(self, ax, drawer, firstColumn):
+        print(firstColumn)
         actorPosition = self.actor.getPosition()
-        newposition = actorPosition.mix(direction.value)
-        if self.tiles[newposition.getX()][newposition.getY()] == True:
-            self.actor.move(direction.value)
+        while(self.getTile(actorPosition.getX(),actorPosition.getY()) == True):
+            drawer.draw_hex_grid(ax, self)
+            plt.draw()
+            plt.pause(1)
+            if(self.actor.getPosition().getX() % 2 == 0):
+                direction01 = coordinate.DirectionEven.SW
+                direction02 = coordinate.DirectionEven.NE
+                direction03 = coordinate.DirectionEven.SE
+                direction04 = coordinate.DirectionEven.N
+                direction05 = coordinate.DirectionEven.NW
+                direction06 = coordinate.DirectionEven.S
+            else:
+                direction01 = coordinate.DirectionOdd.SW
+                direction02 = coordinate.DirectionOdd.NE
+                direction03 = coordinate.DirectionOdd.SE
+                direction04 = coordinate.DirectionOdd.N
+                direction05 = coordinate.DirectionOdd.NW
+                direction06 = coordinate.DirectionOdd.S
+            newPosition01 = actorPosition.mix(direction01.value)
+            newPosition02 = actorPosition.mix(direction02.value)
+            newPosition03 = actorPosition.mix(direction03.value)
+            newPosition04 = actorPosition.mix(direction04.value)
+            if(firstColumn and self.getTile(newPosition01.getX(),newPosition01.getY()) == True):
+                self.actor.move(direction01.value)
+            elif(self.getTile(newPosition02.getX(),newPosition02.getY()) == True and self.getTile(newPosition03.getX(),newPosition03.getY()) == False):
+                self.actor.move(direction03.value)
+                self.actor.move(direction05.value)
+            elif(self.getTile(newPosition04.getX(),newPosition04.getY()) == True and self.getTile(newPosition03.getX(),newPosition03.getY()) == True and self.getTile(newPosition02.getX(),newPosition02.getY()) == False):
+                self.actor.move(direction02.value)
+                self.actor.move(direction01.value)
+            else:
+                self.actor.move(direction06.value)
+        if(self.actor.getPosition().getX() % 2 == 0):
+            direction01 = coordinate.DirectionEven.N
+            direction02 = coordinate.DirectionEven.NE
+            direction03 = coordinate.DirectionEven.SE
+        else:
+            direction01 = coordinate.DirectionOdd.N
+            direction02 = coordinate.DirectionOdd.NE
+            direction03 = coordinate.DirectionOdd.SE
+        newPosition01 = actorPosition.mix(direction01.value)
+        newPosition02 = actorPosition.mix(direction02.value)
+        newPosition03 = actorPosition.mix(direction03.value)
+        if(self.getTile(newPosition01.getX(),newPosition01.getY()) == True and self.getTile(newPosition02.getX(),newPosition02.getY()) == True and self.getTile(newPosition03.getX(),newPosition03.getY()) == True):
+            self.actor.move(direction01.value)
+        elif(self.getTile(newPosition02.getX(),newPosition02.getY()) == True):
+            self.actor.move(direction02.value)
+            self.actor.move(direction01.value)
+            firstColumn = False
+
+
+
+            
+    
+                
